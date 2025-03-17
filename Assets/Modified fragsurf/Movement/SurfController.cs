46d13ac569ee06ea8e141a5064d69541078a156f
damianfel7 <49472570+damianfel7@users.noreply.github.com>
2025-03-18 00:15:54 +0100

    hookjump gravity fix

--- Assets/Modified fragsurf/Movement/SurfController.cs
+++ Assets/Modified fragsurf/Movement/SurfController.cs
@@ -328,13 +328,12 @@ namespace Fragsurf.Movement {
 
         public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
         {
-            float gravity = Physics.gravity.y;
+            float gravity = Physics.gravity.y-10f;
             float displacementY = endPoint.y - startPoint.y;
             Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
 
             Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
-            Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
-                + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+            Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity) + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
 
             return velocityXZ + velocityY;
         }
