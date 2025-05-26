8bff72eacb4d3068bc599f9548ca502bce5ec3ea
damianfel7 <49472570+damianfel7@users.noreply.github.com>
2025-05-26 12:00:07 +0200

    TG-8 drone pathing, more laser wip

--- Assets/DroneController.cs
+++ Assets/DroneController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform player;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float hoverAmplitude = 0.5f;
+    [SerializeField] float hoverFrequency = 1f;
+
+    private Vector3 startPosition;
+
     void Start()
     {
-        
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player")?.transform;
+        }
+
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player == null)
+            return;
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+
+        float hoverOffset = Mathf.Sin(Time.time * hoverFrequency) * hoverAmplitude;
+        transform.position += Vector3.up * hoverOffset * Time.deltaTime;
     }
 }
